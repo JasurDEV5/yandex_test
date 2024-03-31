42d78268bc8edb6705d271c8296a02fa028565f8
Jasur Farmonov <84636901+JasurDEV5@users.noreply.github.com>
2024-03-31 16:57:50 +0500

    test

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const span = document.querySelector('.karaoke span')
+
+window.addEventListener('scroll', () => {
+	const t =
+		window.scrollY /
+		(document.documentElement.scrollHeight - window.innerHeight)
+	span.style.backgroundPosition = `${-t * 100}% 0`
+})
